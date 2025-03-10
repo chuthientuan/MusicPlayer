142338987fc68ab024342e75990b7633ff4db31f
Tuan <tuanchu24112004@gmail.com>
2025-03-10 17:25:43 +0700

    15:00

--- lib/icons_pack/build.gradle.kts
+++ lib/icons_pack/build.gradle.kts
@@ -1,19 +1,16 @@
 plugins {
-    alias(libs.plugins.android.application)
+    alias(libs.plugins.android.library)
 }
 
 android {
-    namespace = "com.example.musicplayer"
+    namespace = "com.example.icons_pack"
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.example.musicplayer"
         minSdk = 24
-        targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
     }
 
     buildTypes {
@@ -35,11 +32,7 @@ dependencies {
 
     implementation(libs.appcompat)
     implementation(libs.material)
-    implementation(libs.activity)
-    implementation(libs.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-
-    implementation(libs.multi.sliding.up.panel)
 }
