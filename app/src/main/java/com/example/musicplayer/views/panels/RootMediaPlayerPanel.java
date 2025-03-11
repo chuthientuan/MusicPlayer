570819109ba58690637461e075d85593640df4a0
Tuan <tuanchu24112004@gmail.com>
2025-03-11 16:59:58 +0700

    23

--- app/src/main/java/com/example/musicplayer/views/panels/RootMediaPlayerPanel.java
+++ app/src/main/java/com/example/musicplayer/views/panels/RootMediaPlayerPanel.java
@@ -1,9 +1,11 @@
 package com.example.musicplayer.views.panels;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 
 import androidx.annotation.NonNull;
 
+import com.example.musicplayer.R;
 import com.realgear.multislidinguppanel.BasePanelView;
 import com.realgear.multislidinguppanel.MultiSlidingUpPanelLayout;
 
@@ -11,11 +13,15 @@ public class RootMediaPlayerPanel extends BasePanelView {
 
     public RootMediaPlayerPanel(@NonNull Context context, MultiSlidingUpPanelLayout panelLayout) {
         super(context, panelLayout);
+        getContext().setTheme(R.style.Theme_MusicPlayer);
+        LayoutInflater.from(getContext()).inflate(R.layout.layout_root_mediaplayer, this, true);
     }
 
     @Override
     public void onCreateView() {
-
+        this.setPanelState(MultiSlidingUpPanelLayout.COLLAPSED);
+        this.setSlideDirection(MultiSlidingUpPanelLayout.SLIDE_VERTICAL);
+        this.setPeakHeight(getResources().getDimensionPixelSize(R.dimen.mediaplayerbar_height));
     }
 
     @Override
